e5259e478ba8585a0c98f5b20953434d6f376bcb
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-17 18:17:48 -0300

    Organizacao Homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Search from './pages/Search';
-import Nav from './pages/Nav';
+import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route to="/nav" component={ Nav } />
         <Route path="/cart" component={ Cart } />
-        <Route exact path="/" component={ Search } />
+        <Route exact path="/" component={ Home } />
       </Switch>
     </BrowserRouter>
   );
