a71f0f47b6d2a7181bf1174ba65cc91adb613306
Michael <michaelcaxias.dev@gmail.com>
2021-09-17 17:08:14 -0300

    Req 3: Completo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Search from './pages/Search';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route to="/" component={ Search } />
+        <Route path="/cart" component={ Cart } />
+        <Route exact path="/" component={ Search } />
       </Switch>
     </BrowserRouter>
   );
