ee2524afc8d30e070c7a74849eb18d2cc3010a8a
Michael <michaelcaxias.dev@gmail.com>
2021-09-25 19:19:32 -0300

    Adiciona Bootstrap

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Product.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Image } from 'semantic-ui-react';
 import AddCart from '../AddCart';
 
 export default class Product extends React.Component {
@@ -11,15 +10,19 @@ export default class Product extends React.Component {
     return (
 
       <div data-testid="product" className="product">
-        <Link
-          className="product-info"
-          to={ `/product/${id}/${encodeURIComponent(title)}` }
-          data-testid="product-detail-link"
-        >
-          <Image className="product-image" src={ thumbnail } alt={ title } />
+        <div className="product-image-container">
+          <Link
+            to={ `/product/${id}/${encodeURIComponent(title)}` }
+            data-testid="product-detail-link"
+          >
+            <img className="product-image" src={ thumbnail } alt={ title } size="small" />
+          </Link>
+        </div>
+        <hr />
+        <div>
           <h4 className="product-price">{`R$ ${price}` }</h4>
           <h3 className="product-title">{title}</h3>
-        </Link>
+        </div>
         <AddCart id={ id } title={ title } price={ price } testId="product-add-to-cart" />
       </div>
 
